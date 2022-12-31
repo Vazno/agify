d21e37021c34785786b5027aedee0da9e49eb759
Beksultan <96925396+Vazno@users.noreply.github.com>
2023-01-01 05:46:38 +0600

    Added support for non async connection

--- README.md
+++ README.md
@@ -3,9 +3,10 @@ A simple API for predicting the age, gender, and country of a person by their na
 
 The API is free for up to 1000 names/day. No sign up or API key needed. So go ahead and try it out.
 ## Usage example:
+### async version:
 ```python
-from agify import NameAPI
-g = NameAPI(["Igor", "Alex"], mode="*")
+from agify import AsyncNameAPI
+g = AsyncNameAPI(["Igor", "Alex"], mode="*")
 print(asyncio.run(g.get_names_info()))
 # ->
 {'Alex': {'age': 45,
@@ -26,18 +27,53 @@ print(asyncio.run(g.get_names_info()))
                       {'country_id': 'SK', 'probability': 0.062}],
           'gender': 'male',
           'probability': 1.0}}
-```
-```python
-a = NameAPI(["Ivan"], "gender")
+
+a = AsyncNameAPI(["Ivan"], "gender")
 print(asyncio.run(a.get_names_info()))
 # ->
 {'Ivan': {'count': 425630, 'gender': 'male', 'probability': 1.0}}
-```
-```python
-a = NameAPI()
+
+a = AsyncNameAPI()
 print(asyncio.run(a.get_limit_remaining()))
 # ->
 987
 ```
+
+### usual version:
+```python
+from agify import NameAPI
+g = NameAPI(["Igor", "Alex"], mode="*")
+print(g.get_names_info())
+# ->
+{'Alex': {'age': 45,
+          'count': 1114390,
+          'country': [{'country_id': 'CZ', 'probability': 0.082},
+                      {'country_id': 'UA', 'probability': 0.045},
+                      {'country_id': 'RO', 'probability': 0.033},
+                      {'country_id': 'RU', 'probability': 0.031},
+                      {'country_id': 'IL', 'probability': 0.028}],
+          'gender': 'male',
+          'probability': 0.96},
+ 'Igor': {'age': 49,
+          'count': 168019,
+          'country': [{'country_id': 'UA', 'probability': 0.169},
+                      {'country_id': 'RS', 'probability': 0.113},
+                      {'country_id': 'RU', 'probability': 0.093},
+                      {'country_id': 'HR', 'probability': 0.084},
+                      {'country_id': 'SK', 'probability': 0.062}],
+          'gender': 'male',
+          'probability': 1.0}}
+
+a = NameAPI(["Ivan"], "gender")
+print(a.get_names_info())
+# ->
+{'Ivan': {'count': 425630, 'gender': 'male', 'probability': 1.0}}
+
+a = NameAPI()
+print(a.get_limit_remaining())
+# ->
+987
+```
+
 ---
 [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
--- agify.py
+++ agify.py
@@ -10,13 +10,17 @@ class NameAPI():
 	NATIONALIZE_URL = "https://api.nationalize.io?"
 	GENDERIZE_URL = "https://api.genderize.io?"
 
+	X_RATE_LIMIT_RESET = "x-rate-limit-reset"
+	X_RATE_LIMIT_LIMIT = "x-rate-limit-limit"
+	X_RATE_LIMIT_REMAINING = "x-rate-limit-remaining"
+
 	def __init__(self, names: Union[Iterable[str], str] = [],
 					mode: Literal["age", "nation", "gender", "*"] = "*",
-					api_key: str = None,
 					country_id: str = None,
+					api_key: str = None,
 					ignore_errors: bool = False) -> None:
-		'''Predict current age by name.
-			An asynchronous Python wrapper for https://agify.io/ https://genderize.io/ https://nationalize.io/
+		'''Predict current age, nation, gender by name.
+			Python wrapper for these API's: https://agify.io/ https://genderize.io/ https://nationalize.io/
 		The API follows ISO 3166-1 alpha-2 for country codes.
 			List https://agify.io/our-data of supported countries.
 		'''
@@ -38,13 +42,117 @@ class NameAPI():
 		self.country_id = f"&country_id={country_id}" if api_key != None else ""
 		self.ignore_errors = bool(ignore_errors)
 
+	def __send_requests(self) -> List[httpx.Response]:
+		with httpx.Client() as client:
+			responses = list()
+			for multi_req_url in self._gen_urls():
+				responses.append(client.get(multi_req_url))
+		return responses
+
+	def get_names_info(self) -> Dict:
+		'''Returns dictionary with info about names'''
+		if self.names == []:
+			raise ValueError("Please provide names to check, names parameter is empty.")
+		dict_ = dict()
+		responses = self.__send_requests()
+
+		if self.ignore_errors is False:
+			self._check_errors(responses)
+			for response in responses:
+				for d in response.json():
+					d_name = d["name"]
+					del d["name"]
+					try:
+						dict_[d_name] = dict_[d_name] | d
+					except KeyError:
+						dict_[d_name] = d
+		else:
+			for response in responses:
+				for d in response.json():
+					try:
+						d_name = d["name"]
+						del d["name"]
+						try:
+							dict_[d_name] = dict_[d_name] | d
+						except KeyError:
+							dict_[d_name] = d
+					except TypeError:
+						pass
+		return dict_
+
+	def get_headers(self) -> httpx.Headers:
+		'''Returns response's headers
+			
+			Each call decreases amount of total available calls by 1'''
+		with httpx.Client() as client:
+			response = client.get(f"{self.URL[0]}&name=Test")
+		return response.headers
+
+	def get_limit_reset(self) -> int:
+		'''Seconds remaining until a new time window opens
+			
+			Each call decreases amount of total available calls by 1'''
+		with httpx.Client() as client:
+			response = client.get(f"{self.URL[0]}&name=Test")
+		return int(response.headers[self.X_RATE_LIMIT_RESET])
+
+	def get_limit(self) -> int:
+		'''The amount of names available in the current time window
+			
+			Each call decreases amount of total available calls by 1'''
+		with httpx.Client() as client:
+			response = client.get(f"{self.URL[0]}&name=Test")
+		return int(response.headers[self.X_RATE_LIMIT_LIMIT])
+
+	def get_limit_remaining(self) -> int:
+		'''The number of names left in the current time window
+			
+			Each call decreases amount of total available calls by 1'''
+		with httpx.Client() as client:
+			response = client.get(f"{self.URL[0]}&name=Test")
+		return int(response.headers[self.X_RATE_LIMIT_REMAINING])
+
+	def _check_errors(self, responses: List[httpx.Response]) -> None:
+		'''Checks if there are any errors in request.'''
+		for response in responses:
+			if response.status_code != 200:
+				raise ValueError(response.json()["error"])
+
+	def _gen_urls(self) -> List[str]:
+		'''Generates urls with MAX_PER_REQUEST names in single url'''
+		url_lists = list()
+		for url in self.URL:
+			names = copy(self.names)
+			while names != []:
+				for name in range(self.MAX_PER_REQUEST):
+					try:
+						url += f"&name[]={names.pop(0)}"
+					except IndexError:
+						break
+				url += self.api_key + self.country_id
+				url_lists.append(url)
+		return url_lists
+
+class AsyncNameAPI(NameAPI):
+	def __init__(self, names: Union[Iterable[str], str] = [],
+					mode: Literal["age", "nation", "gender", "*"] = "*",
+					country_id: str = None,
+					api_key: str = None,
+					ignore_errors: bool = False) -> None:
+		'''Predict current age, nation, gender by name.
+			An asynchronous Python wrapper for these API's: https://agify.io/ https://genderize.io/ https://nationalize.io/
+		The API follows ISO 3166-1 alpha-2 for country codes.
+			List https://agify.io/our-data of supported countries.
+		'''
+		super().__init__(names, mode, country_id, api_key, ignore_errors)
+
 	async def __send_requests(self) -> List[httpx.Response]:
 		async with httpx.AsyncClient() as client:
 			tasks = list()
-			for multi_req_url in self.__gen_urls():
+			for multi_req_url in super()._gen_urls():
 				tasks.append(asyncio.create_task(client.get(multi_req_url)))
-			t = await asyncio.gather(*tasks)
-		return t
+			responses = await asyncio.gather(*tasks)
+		return responses
 
 	async def get_names_info(self) -> Dict:
 		'''Returns dictionary with info about names'''
@@ -54,7 +162,7 @@ class NameAPI():
 		responses = await self.__send_requests()
 
 		if self.ignore_errors is False:
-			self.__check_errors(responses)
+			super()._check_errors(responses)
 			for response in responses:
 				for d in response.json():
 					d_name = d["name"]
@@ -91,7 +199,7 @@ class NameAPI():
 			Each call decreases amount of total available calls by 1'''
 		async with httpx.AsyncClient() as client:
 			response = await client.get(f"{self.URL[0]}&name=Test")
-		return int(response.headers["x-rate-limit-reset"])
+		return int(response.headers[self.X_RATE_LIMIT_RESET])
 
 	async def get_limit(self) -> int:
 		'''The amount of names available in the current time window
@@ -99,7 +207,7 @@ class NameAPI():
 			Each call decreases amount of total available calls by 1'''
 		async with httpx.AsyncClient() as client:
 			response = await client.get(f"{self.URL[0]}&name=Test")
-		return int(response.headers["x-rate-limit-limit"])
+		return int(response.headers[self.X_RATE_LIMIT_LIMIT])
 
 	async def get_limit_remaining(self) -> int:
 		'''The number of names left in the current time window
@@ -107,25 +215,4 @@ class NameAPI():
 			Each call decreases amount of total available calls by 1'''
 		async with httpx.AsyncClient() as client:
 			response = await client.get(f"{self.URL[0]}&name=Test")
-		return int(response.headers["x-rate-limit-remaining"])
-
-	def __check_errors(self, responses: List[httpx.Response]) -> None:
-		'''Checks if there are any errors in request.'''
-		for response in responses:
-			if response.status_code != 200:
-				raise ValueError(response.json()["error"])
-
-	def __gen_urls(self):
-		'''Generates urls with MAX_PER_REQUEST names in single url'''
-		url_lists = list()
-		for url in self.URL:
-			names = copy(self.names)
-			while names != []:
-				for name in range(self.MAX_PER_REQUEST):
-					try:
-						url += f"&name[]={names.pop(0)}"
-					except IndexError:
-						break
-				url += self.api_key + self.country_id
-				url_lists.append(url)
-		return url_lists
+		return int(response.headers[self.X_RATE_LIMIT_REMAINING])
